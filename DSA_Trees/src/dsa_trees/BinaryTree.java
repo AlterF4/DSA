dd93d8fdb61e0ffd54257460ddf2ad382f317860
Prasith <prasithrcg@gmail.com>
2014-02-20 20:50:08 +0530

    Binary Tree Part 2
    Replace the Node in BinaryTree.java->line 126 with relevent traversal
    Code

--- DSA_Trees/src/dsa_trees/BinaryTree.java
+++ DSA_Trees/src/dsa_trees/BinaryTree.java
@@ -60,4 +60,90 @@ public class BinaryTree {
         }
 
     }
+
+
+// Delete nodes from the tree
+    public boolean delete(int value) {
+
+        Node nodeToDelete = find(value);
+
+        if (nodeToDelete != null) {
+            if (nodeToDelete.getLeftChild() == null && nodeToDelete.getRightChild() == null) {
+                // If leftchild and rightChild doesnot exsit in the tree
+                delcase1(nodeToDelete);
+            } else if (nodeToDelete.getLeftChild() != null) {
+                // In case where leftChild should be Deleted
+                delcase2(nodeToDelete);
+            } else if (nodeToDelete.getRightChild() != null && nodeToDelete.getLeftChild() != null) {
+                // In case where rightChild Should be Deleted 
+                delcase3(nodeToDelete);
+            } else if (nodeToDelete.getRightChild() != null) {
+                delcase2(nodeToDelete);
+            }
+        }
+
+        return false;
+    }
+
+    private void delcase1(Node nodeToDelete) {
+
+        if (nodeToDelete.getParent().getLeftChild() == nodeToDelete) {
+            //Deleting Left Child
+            nodeToDelete.getParent().setLeftChild(null);
+
+        } else if (nodeToDelete.getParent().getRightChild() == nodeToDelete) {
+            //Deleting Right child
+            nodeToDelete.getParent().setRightChild(null);
+
+        }
+    }
+    
+  
+    private void delcase2(Node nodeToDelete) {
+
+        if (nodeToDelete.getParent().getLeftChild() == nodeToDelete) {
+
+            if (nodeToDelete.getLeftChild() != null) {
+
+                nodeToDelete.getParent().setLeftChild(nodeToDelete.getLeftChild());
+
+            } else if (nodeToDelete.getRightChild() != null) {
+                nodeToDelete.getParent().setLeftChild(nodeToDelete.getRightChild());
+            }
+        } else if (nodeToDelete.getParent().getRightChild() == nodeToDelete) {
+
+            if (nodeToDelete.getLeftChild() != null) {
+
+                nodeToDelete.getParent().setRightChild(nodeToDelete.getLeftChild());
+
+            } else if (nodeToDelete.getRightChild() != null) {
+                nodeToDelete.getParent().setRightChild(nodeToDelete.getRightChild());
+            }
+
+        }
+    }
+
+    private void delcase3(Node nodeToDelete) {
+        Node DelNode= new Node (a.getRightChild());
+ /* Add the traversal code here and delete above node  */
+
+        delcase2(DelNode);
+
+        DelNode.setParent(nodeToDelete.getParent());
+        DelNode.setLeftChild(nodeToDelete.getLeftChild());
+        DelNode.setRightChild(nodeToDelete.getRightChild());
+
+        if (nodeToDelete.getParent() == null) {
+            root = DelNode;
+        } else {
+            if (nodeToDelete.getParent().getLeftChild() == nodeToDelete) {
+
+                nodeToDelete.getParent().setLeftChild(DelNode);
+
+            } else if (nodeToDelete.getParent().getRightChild() == nodeToDelete) {
+
+                nodeToDelete.getParent().setRightChild(DelNode);
+            }
+        }
+    }
 }
